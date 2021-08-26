08cf0e6def3139c32d4236e5e8284ab513ea06e9
JIYIFENG-SO <75471540+JIYIFENG-SO@users.noreply.github.com>
2021-08-26 11:42:49 +0800

    排序之冒泡排序
    要求将给定的n个整数从小到大排序后输出

---
+++ 排序之冒泡排序.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void){
+    int n,i,j,max,temp;
+    int a[1000];
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+        scanf("%d",&a[i]);
+    for(i=1;i<n;i++)
+       for(j=n;j>i;j--)
+         if(a[j]<a[j-1])
+            temp=a[j],a[j]=a[j-1],a[j-1]=temp;
+    for(i=1;i<n;i++)
+        printf("%d ",a[i]);
+    printf("%d\n",a[n]);
+    return 0;
+}
+/*要求将给定的n个整数从小到大排序后输出*/ 
